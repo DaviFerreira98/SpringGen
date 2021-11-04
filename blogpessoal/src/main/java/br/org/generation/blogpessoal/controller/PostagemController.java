410a852f5d9e7ac6e5699d6ee332bb0ec013409f
DaviFerreira98 <davi2803ferreira@gmail.com>
2021-11-03 22:13:10 -0300

    Segunda parte do Blog

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
@@ -3,9 +3,14 @@ package br.org.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +28,40 @@ public class PostagemController {
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
 		return ResponseEntity.ok(postagemRepository.findAll());
+		//select * from tb_postagens
 	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+		//select * from tb_postagens where id=1;
+	}
+	@GetMapping("titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	//Inserir um novo recurso
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	//Atualizar
+	@PostMapping
+		public ResponseEntity<Postagem> putPustagem(@RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.ok(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletaPostagem(@PathVariable long id) {
+        if (!postagemRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        postagemRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+	
 }
